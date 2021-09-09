fbdc93012dabeaf8aa4afcb856cc9b31a0c53f28
Marc-Alban <marcalban@live.fr>
2021-09-09 10:42:19 +0200

    Add Website with js and little jquery for training function

---
+++ js/contactForm.js
@@ -0,0 +1,45 @@
+let contactForm = document.getElementById('frmContact');
+let fields = document.querySelectorAll("input[required],textarea[required]");
+
+fields.forEach((field) => {
+    field.addEventListener('focus', () => { resetField(field) }, false);
+    field.addEventListener('blur', () => { validateField(field) }, false);
+});
+
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fields.forEach((field) => { resetField(field) });
+    let valide = true;
+
+    fields.forEach((field) => {
+        if (!validateField(field)) {
+            valide = false;
+        }
+    });
+
+    if (valide) {
+        e.target.submit();
+    }
+
+
+}, false);
+
+function validateField(field) {
+    if (field.checkValidity()) {
+        return true;
+    } else {
+        field.classList.add('invalid');
+        field.previousElementSibling.insertAdjacentHTML('beforeend', `<span class="msg">${field.validationMessage}</span>`);
+        return false;
+    }
+}
+
+function resetField(field) {
+    let fieldLabel = field.previousElementSibling;
+    field.classList.remove('invalid');
+    while (fieldLabel.firstElementChild) {
+        fieldLabel.removeChild(fieldLabel.firstElementChild);
+    }
+    field.valide = true;
+}
